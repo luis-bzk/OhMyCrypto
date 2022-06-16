4811496030d4ea07b639836b7869e9c90b5cfe8b
luis <laberrezueta2@protonmail.com>
2022-06-16 12:26:27 -0500

    colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className={"container app-layout"}>
       <img className={"crypto-img"} src={cryptoImg} alt="img crypto" />
       <div className={"app-content"}>
-        <h1>
+        <h1 className={"content-title"}>
           Quote a currency!
         </h1>
       </div>
